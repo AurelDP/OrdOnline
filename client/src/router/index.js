54451d8914d1243ebb4abfde5bb788ef70a3e0a2
Aurélien <clieledp@gmail.com>
2022-06-22 17:27:31 +0200

    [Update] Error404 page in router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Error404 from '../views/ErrorVue.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
+  {path: '/', name: 'home', component: HomeView},
+  {path: '/about', name: 'about', component: AboutView},
+  {path: "/:catchAll(.*)*", component: Error404},
 ]
 
 const router = createRouter({
