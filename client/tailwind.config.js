4e50030d67d53f798d6a5df4efe11de8589949e1
Aurélien <aureliendp@free.fr>
2022-06-22 17:21:24 +0200

    [Update] Added colors to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'ord-dark-green': '#073B3A',
+      'ord-green': {
+        100: '#00A99D',
+        200: '#41C2B2',
+        300: '#3DC7B9',
+        400: '#4BCFC1',
+        500: '#AEE4E3',
+        600: '#E4F8F5',
+      },
+      'ord-dark-blue': '#2D3047',
+      'ord-red': '#DE5456',
+    }
   },
   plugins: [],
 }
