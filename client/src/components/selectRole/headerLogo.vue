fa38dc73f362a727f30e08182fa5973ea49a5c75
bastien707 <bastien.facqueur@efrei.net>
2022-06-23 22:23:32 +0200

    [Update] switch to svg && made logo clickable

--- client/src/components/selectRole/headerLogo.vue
+++ client/src/components/selectRole/headerLogo.vue
@@ -1,10 +1,16 @@
 <template>
-  <img class="mx-auto md:max-w-md mb-10" src="@/assets/headerLogo/banniere.png" alt="banniere"/>
+  <img class="mx-auto md:max-w-md mb-10 hover:cursor-pointer" src="@/assets/headerLogo/banniere.svg" alt="banniere" @click="goToHome()"/>
 </template>
 
 <script>
 export default {
-  name: "headerLogo"
+  name: "headerLogo",
+  methods:{
+    goToHome()
+    {
+      this.$router.push('/');
+    }
+  }
 }
 </script>
 
