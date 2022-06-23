a8dd0530c281c39d39592505b36f9564b227c8d1
bastien707 <bastien.facqueur@efrei.net>
2022-06-23 20:42:10 +0200

    [Add] new select role page

---
+++ client/src/views/selectRole/SelectRoleView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="px-24 py-10 ord-background-gradient">
+    <HeaderLogo/>
+  <div class="flex flex-col place-content-center">
+    <h5 class="mt-4 md:flex items-start text-4xl font-black text-ord-dark-blue">S'inscrire</h5>
+    <h5 class="mt-4 md:flex items-start text-2xl text-ord-dark-blue">Vous êtes</h5>
+    <div class="flex flex-col items-center md:items-center md:flex-row sm:flex-col sm:items-center place-content-center">
+      <CardRole role="patient"/>
+      <CardRole role="doctor"/>
+      <CardRole role="pharma"/>
+      <CardRole role="healthService"/>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+import CardRole from "@/components/selectRole/cardRole";
+import HeaderLogo from "@/components/selectRole/headerLogo";
+
+export default {
+  name: "selectRole",
+  components: {HeaderLogo, CardRole}
+}
+</script>
+
+<style scoped>
+
+</style>
