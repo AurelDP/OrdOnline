85e9329b2ecba2a5c2285bfe2221eec09533cd11
ksauvee <kylian@sauvee.com>
2022-06-23 18:43:53 +0200

    [Feat] Add RegisterView

---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg ord-background-gradient">
+    <RegisterForm></RegisterForm>
+  </div>
+</template>
+
+<script>
+import RegisterForm from "@/components/RegisterForm";
+
+export default {
+  name: "RegisterView",
+  components: {RegisterForm}
+}
+</script>
+
+<style scoped>
+.bg {
+  width: 1920px;
+}
+</style>
