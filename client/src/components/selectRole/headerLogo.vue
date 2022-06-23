637bc407a60584227010e14b0a2d192e1775a08d
bastien707 <bastien.facqueur@efrei.net>
2022-06-23 20:39:38 +0200

    [Add] new component for responsive logo

---
+++ client/src/components/selectRole/headerLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <img class="mx-auto md:max-w-md mb-10" src="@/assets/headerLogo/banniere.png" alt="banniere"/>
+</template>
+
+<script>
+export default {
+  name: "headerLogo"
+}
+</script>
+
+<style scoped>
+
+</style>
