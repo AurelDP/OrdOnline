bc12bc457e2c93f2e834106477959d0b3e400130
bastien707 <bastien.facqueur@efrei.net>
2022-06-23 20:41:28 +0200

    [Update] add new route toward selection role page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Error404 from '../views/ErrorView.vue'
+import SelectRoleView from "@/views/selectRole/SelectRoleView";
 
 const routes = [
   {path: '/', name: 'home', component: HomeView},
   {path: '/about', name: 'about', component: AboutView},
+  {path: '/select-role', name: 'role', component: SelectRoleView},
   {path: "/:catchAll(.*)*", component: Error404},
 ]
 
