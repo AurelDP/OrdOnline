29f6925596efc1017cd1496fac6dbe36e6a6a8f1
bastien707 <bastien.facqueur@efrei.net>
2022-06-24 10:26:59 +0200

    [Fix] place cards evenly

--- client/src/views/selectRole/SelectRoleView.vue
+++ client/src/views/selectRole/SelectRoleView.vue
@@ -4,7 +4,7 @@
   <div class="flex flex-col place-content-center">
     <h5 class="mt-4 md:flex items-start text-4xl font-black text-ord-dark-blue">S'inscrire</h5>
     <h5 class="mt-4 md:flex items-start text-2xl text-ord-dark-blue">Vous êtes</h5>
-    <div class="flex flex-col items-center md:items-center md:flex-row sm:flex-col sm:items-center place-content-center">
+    <div class="flex flex-col flex-wrap items-center md:items-center md:flex-row sm:flex-col sm:items-center place-content-evenly">
       <CardRole role="patient"/>
       <CardRole role="doctor"/>
       <CardRole role="pharma"/>
