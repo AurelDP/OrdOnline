b20e945317a65baf66ee0f3fc33139d3062f76cf
ksauvee <kylian@sauvee.com>
2022-06-24 11:54:31 +0200

    [Feat] Add WhiteBoard component

---
+++ client/src/components/globalComponents/WhiteBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-screen">
+    <div class="md:p-14 sm:p-10 p-6 md:m-44 sm:m-24 m-12 bg-white shadow-lg">
+      <h1 class="text-3xl font-bold ord-dark-blue">{{ title }}</h1>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WhiteBoard",
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
