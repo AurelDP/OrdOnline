e45b9d93e7cb51f91d082179a12e07aa736d46f8
bastien707 <bastien.facqueur@efrei.net>
2022-06-27 21:01:53 +0200

    [Update] new route toward customer space view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,12 +4,14 @@ import AboutView from '../views/AboutView.vue'
 import Error404 from '../views/ErrorView.vue'
 import SelectRoleView from "@/views/SelectRoleView";
 import RegisterView from "../views/RegisterView";
+import CustomerSpaceView from "@/views/CustomerSpaceView";
 
 const routes = [
   {path: '/', name: 'home', component: HomeView},
   {path: '/about', name: 'about', component: AboutView},
   {path: '/signUp', name: 'role', component: SelectRoleView},
   {path: '/register', name: 'register', component: RegisterView},
+  {path: '/customer-space', name: 'customer', component: CustomerSpaceView},
   {path: "/:catchAll(.*)*", component: Error404},
 ]
 
