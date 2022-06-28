a28b2d2f0d764df92f38acda7a2d957037939212
Joel <joelgabbai@gmail.com>
2022-06-28 17:57:07 +0200

    [Update] add responsive

---
+++ client/src/components/Accueil/bulle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="self-center">
+    <div class="flex justify-center bg-ord-green-600 w-64 h-64 rounded-full shadow-xl bg-opacity-50">
+      <img class="w-48" v-bind:src="path" alt="Secure image">
+    </div>
+    <h1 class="mt-6 text-2xl font-extrabold">{{blackWord}}<br><span class="text-ord-green-200">{{ greenWord }}</span></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blackWord: String,
+    greenWord: String,
+    path: String
+  }
+}
+</script>
+
+<style>
+</style>
