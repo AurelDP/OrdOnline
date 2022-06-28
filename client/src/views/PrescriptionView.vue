f05cb7b3d30fbd2f440dd9208224c9d0e61dd43c
ksauvee <kylian@sauvee.com>
2022-06-28 18:58:24 +0200

    [Feat] Add Prescription page

---
+++ client/src/views/PrescriptionView.vue
@@ -0,0 +1,37 @@
+<template>
+  <AdaptFooterBackground :backgroundGradient="true">
+    <Navbar/>
+    <WhiteBoard
+        title="Ordonnance"
+        :button1="true"
+        :class1="'ord-button-green hover:ord-button-green-hover'"
+        :text1="'Retour'"
+        :src1="'/'"
+        :button2="true"
+        :class2="'ord-button-green hover:ord-button-green-hover'"
+        :text2="'Modifier'"
+        :src2="'/'"
+    >
+    </WhiteBoard>
+    <WhiteBoard></WhiteBoard>
+  </AdaptFooterBackground>
+</template>
+
+<script>
+import AdaptFooterBackground from "@/components/globalComponents/AdaptFooterBackground";
+import Navbar from "@/components/globalComponents/Navbar";
+import WhiteBoard from "@/components/globalComponents/WhiteBoard";
+
+export default {
+  name: "PrescriptionView",
+  components: {
+    AdaptFooterBackground,
+    Navbar,
+    WhiteBoard
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
