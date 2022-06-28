7f8970b5a349f5e6b357d91a79e7d097d23a3f56
ksauvee <kylian@sauvee.com>
2022-06-29 00:01:07 +0200

    [Feat] Add Prescription to Prescription page

---
+++ client/src/components/prescriptionPage/Prescription.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="bg-ord-red float-right bg-gray-200 py-1 px-5 shadow-lg">
+    <p>Terminée</p>
+  </div>
+  <div class="my-10 md:text-base text-sm">
+    <h2 class="text-2xl">Médecin</h2>
+    <p class="my-5 px-5 border-l-2 border-ord-green-400">Patrick Chaumont, 4 Rue Charles Péguy, Palaiseau, 91120</p>
+  </div>
+  <div class="my-10 md:text-base text-sm">
+    <h2 class="text-2xl">Patient</h2>
+    <p class="my-5 px-5 border-l-2 border-ord-green-400">Jean Dupont, [âge si renseigné], [poids si renseigné]</p>
+  </div>
+  <div class="my-10 md:text-base text-sm">
+    <h2 class="text-2xl">Traitements (2)</h2>
+    <ul>
+      <li class="my-5 py-1 border-b-2">
+        <div class="flex flex-wrap">
+          <p class="w-full md:w-1/2">Doliprane paracétamol 500mg 16 gélules</p>
+          <p class="w-full md:w-1/2 md:text-right">Substituable, Remboursable</p>
+        </div>
+        <p>[Description du traitement si besoin]</p>
+        <div class="flex flex-wrap">
+          <p class="w-full md:w-1/2">[Modalités de renouvellement si besoin]</p>
+          <div class="w-full md:w-1/2 md:text-right">
+            <label for="delivered" class="mr-5">Delivré</label>
+            <input type="checkbox" id="delivered" name="delivered" checked/>
+          </div>
+        </div>
+      </li>
+      <li class="my-5 py-1 border-b-2">
+        <div class="flex flex-wrap">
+          <p class="w-full md:w-1/2">Bétaméthasone Biogaran 30g 0.05% crème</p>
+          <p class="w-full md:w-1/2 md:text-right">Non substituable, Remboursable</p>
+        </div>
+        <p>2 fois/jour pendant 2 semaines, puis espacer les prises (1/jours, 1/2jours, 1/3 jours) jusqu'à arrêt complet</p>
+        <div class="flex flex-wrap">
+          <p class="w-full md:w-1/2"></p>
+          <div class="w-full md:w-1/2 md:text-right">
+            <label for="delivered" class="mr-5">Delivré</label>
+            <input type="checkbox" id="delivered" name="delivered" checked/>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div class="my-10 md:text-base text-sm">
+    <h2 class="text-2xl">Conseils médicaux</h2>
+    <p class="my-5 px-5 border-l-2 border-ord-green-400">Aucuns</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Prescription"
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/prescriptionPage/PrescriptionStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="my-8 md:text-base text-sm">
+    <li class="flex border-b-2">
+      <p class="w-1/3">07/02/2022</p>
+      <p class="w-1/3 text-ord-green-400">En cours</p>
+      <p class="w-1/3 text-right">Pharmacie de Lozère</p>
+    </li>
+    <li class="flex border-b-2 my-2">
+      <p class="w-1/3">14/02/2022</p>
+      <p class="w-1/3">Terminée</p>
+      <p class="w-1/3 text-right">Grande pharmacie de Villebon</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "PrescriptionStatus"
+}
+</script>
+
+<style scoped>
+
+</style>
