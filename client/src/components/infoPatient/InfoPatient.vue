1922143a5f2437898c236fb9b58b5693d44fd518
Aurélien <aureliendp@free.fr>
2022-06-29 11:12:20 +0200

    [Add] infoPatient page

---
+++ client/src/components/infoPatient/InfoPatient.vue
@@ -0,0 +1,20 @@
+<template>
+ <WhiteBoard
+  :title="'Aurélien Duval'"
+ >
+
+ </WhiteBoard>
+</template>
+
+<script>
+import WhiteBoard from "@/components/globalComponents/WhiteBoard";
+
+export default {
+  name: "InfoPatient.vue",
+  components: {WhiteBoard}
+}
+</script>
+
+<style scoped>
+
+</style>
