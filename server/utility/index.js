b1656f03a6e3b9e914b910f5ef4eac43b628e6bf
Aurélien <clieledp@gmail.com>
2022-06-30 21:14:46 +0200

    [Debug] Working on database

--- server/utility/index.js
+++ server/utility/index.js
@@ -1,10 +1,23 @@
-const getPgClient = require('./connectServer');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mySQLClient = mysql.createConnection({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    ssl: true
+});
+
+console.log(mySQLClient);
 
 const tryToConnect = () => {
-    const newClient = getPgClient();
-    newClient.connect();
+    mySQLClient.connect(function(err) {
+        if (err) throw err;
+    });
 
-    return newClient;
+    return mySQLClient;
 };
 
 module.exports = {
