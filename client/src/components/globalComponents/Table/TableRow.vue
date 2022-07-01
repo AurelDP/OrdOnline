fb18d81fb720b39fd7fe010c265b1da81c1e6794
bastien707 <bastien.facqueur@efrei.net>
2022-07-01 18:42:14 +0200

    [Update] add new prescription list and some rearrangement

--- client/src/components/globalComponents/Table/TableRow.vue
+++ client/src/components/globalComponents/Table/TableRow.vue
@@ -1,11 +1,23 @@
 <template>
-  <tr class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
+  <tr v-if="type === 'my_prescription'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
     <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
     <td class="p-3 whitespace-nowrap">{{ val2 }}</td>
     <td v-if="val3 === 'En attente'" class="py-3 px-8 whitespace-nowrap text-right text-ord-lightred">{{ val3 }}</td>
-    <td v-else-if="val3 === 'En cours'" class="py-3 px-8 whitespace-nowrap text-right text-ord-green-100">{{ val3 }}</td>
-    <td v-else-if="val3 === 'Terminée'" class="py-3 px-8 whitespace-nowrap text-right">{{ val3 }}</td>
-    <td v-else-if="(type === 'ord' || type === 'doctor')" class="py-3 px-8 whitespace-nowrap text-right">{{ val3 }}</td>
+    <td v-if="val3 === 'En cours'" class="py-3 px-8 whitespace-nowrap text-right text-ord-green-100">{{ val3 }}</td>
+    <td v-if="val3 === 'Terminée'" class="py-3 px-8 whitespace-nowrap text-right">{{ val3 }}</td>
+  </tr>
+
+  <tr v-if="type === 'my_doctor' || type === 'my_pharmacy'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
+    <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
+    <td class="p-3 whitespace-nowrap">{{ val2 }}</td>
+    <td v-if="type === 'my_doctor'" class="p-3 whitespace-nowrap">{{ val3 }}</td>
+  </tr>
+
+  <tr v-if="type === 'prescriptions'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
+    <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
+    <td v-if="val2 === 'En attente'" class="py-3 px-8 whitespace-nowrap text-right text-ord-lightred">{{ val2 }}</td>
+    <td v-if="val2 === 'En cours'" class="py-3 px-8 whitespace-nowrap text-right text-ord-green-100">{{ val2 }}</td>
+    <td v-if="val2 === 'Terminée'" class="py-3 px-8 whitespace-nowrap text-right">{{ val2 }}</td>
   </tr>
 </template>
 
