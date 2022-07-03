0a2e79b86fbe7d265344f1b5691a61da56a64a5d
Aurélien <clieledp@gmail.com>
2022-07-03 20:08:33 +0200

    [Update] Modal + 4 register pages finished

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPlus, faUserPlus, faArrowRightToBracket, faArrowRightFromBracket, faInfo, faWarning } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faUserPlus, faArrowRightToBracket, faArrowRightFromBracket, faInfo, faWarning, faCheck } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faPlus, faUserPlus, faArrowRightToBracket, faArrowRightFromBracket, faInfo, faWarning)
+library.add(faPlus, faUserPlus, faArrowRightToBracket, faArrowRightFromBracket, faInfo, faWarning, faCheck)
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- server/repository/pharmaRepository.js
+++ server/repository/pharmaRepository.js
@@ -1,5 +1,5 @@
 async function save(pool, name, rppsNumber, addressId, accountId) {
-    const insertUserQuery = `INSERT INTO Pharmacie (nomPharmacie, numeroRPPSpharmacie, IDadresse, IDcompte)
+    const insertUserQuery = `INSERT INTO Pharmacie (nomPharmacie, numeroRPPSpharmacien, IDadresse, IDcompte)
                              VALUES ('${name}', '${rppsNumber}', ${addressId}, ${accountId})`;
     return await pool.promise().query(insertUserQuery);
 }
