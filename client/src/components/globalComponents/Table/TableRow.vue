8f07baf3ec38c8f9ce70c5ac152f6a5fd1c1573e
Aurélien <aureliendp@free.fr>
2022-07-03 12:16:05 +0200

    [Update] Shadow on patient space page

--- client/src/components/globalComponents/Table/TableRow.vue
+++ client/src/components/globalComponents/Table/TableRow.vue
@@ -2,22 +2,22 @@
   <tr v-if="type === 'my_prescription'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
     <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
     <td class="p-3 whitespace-nowrap">{{ val2 }}</td>
-    <td v-if="val3 === 'En attente'" class="py-3 px-8 whitespace-nowrap text-right text-ord-lightred">{{ val3 }}</td>
-    <td v-if="val3 === 'En cours'" class="py-3 px-8 whitespace-nowrap text-right text-ord-green-100">{{ val3 }}</td>
-    <td v-if="val3 === 'Terminée'" class="py-3 px-8 whitespace-nowrap text-right">{{ val3 }}</td>
+    <td v-if="val3 === 'En attente'" class="p-3 pr-8 whitespace-nowrap text-right text-ord-lightred">{{ val3 }}</td>
+    <td v-if="val3 === 'En cours'" class="p-3 pr-8 whitespace-nowrap text-right text-ord-green-100">{{ val3 }}</td>
+    <td v-if="val3 === 'Terminée'" class="p-3 pr-8 whitespace-nowrap text-right">{{ val3 }}</td>
   </tr>
 
   <tr v-if="type === 'my_doctor' || type === 'my_pharmacy'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
     <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
     <td class="p-3 whitespace-nowrap">{{ val2 }}</td>
-    <td v-if="type === 'my_doctor'" class="p-3 whitespace-nowrap">{{ val3 }}</td>
+    <td v-if="type === 'my_doctor'" class="p-3 pr-8 whitespace-nowrap">{{ val3 }}</td>
   </tr>
 
   <tr v-if="type === 'prescriptions'" class="border-b-2 hover:bg-ord-green-600 hover:cursor-pointer" @click="goTo">
     <td class="p-3 whitespace-nowrap">{{ val1 }}</td>
-    <td v-if="val2 === 'En attente'" class="py-3 px-8 whitespace-nowrap text-right text-ord-lightred">{{ val2 }}</td>
-    <td v-if="val2 === 'En cours'" class="py-3 px-8 whitespace-nowrap text-right text-ord-green-100">{{ val2 }}</td>
-    <td v-if="val2 === 'Terminée'" class="py-3 px-8 whitespace-nowrap text-right">{{ val2 }}</td>
+    <td v-if="val2 === 'En attente'" class="p-3 pr-8 whitespace-nowrap text-right text-ord-lightred">{{ val2 }}</td>
+    <td v-if="val2 === 'En cours'" class="p-3 pr-8 whitespace-nowrap text-right text-ord-green-100">{{ val2 }}</td>
+    <td v-if="val2 === 'Terminée'" class="p-3 pr-8 whitespace-nowrap text-right">{{ val2 }}</td>
   </tr>
 </template>
 
