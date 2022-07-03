9bde97223765be31fe2ad534d9efa095f77d36aa
Aurélien <aureliendp@free.fr>
2022-07-03 15:34:18 +0200

    [Fix] Back connection + mysql2

---
+++ server/repository/homeRepository.js
@@ -0,0 +1,25 @@
+const utility = require("../utility/index");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DATABASE_NAME = process.env.DATABASE;
+const TABLE_NAME = "AccesOrdo";
+
+const TABLE_PATH = DATABASE_NAME + "." + TABLE_NAME;
+
+const getAll = async () => {
+    const client = utility.mySQLClient;
+    const sqlQuery = `SELECT * FROM ${TABLE_PATH}`;
+
+    try {
+        const results = await client.promise().query(sqlQuery);
+        return results[0];
+    } catch(err) {
+        console.log(err.stack);
+        return null;
+    }
+}
+
+module.exports = {
+    getAll
+}
