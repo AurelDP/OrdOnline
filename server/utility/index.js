cfab8ebb0664b462eba11241b3739305df1cd4c4
Aurélien <clieledp@gmail.com>
2022-07-03 15:34:18 +0200

    [Fix] Back connection + mysql2

--- server/utility/index.js
+++ server/utility/index.js
@@ -1,14 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const mySQLClient = mysql.createPool({
-    user: process.env.USERNAME,
+    user: process.env.DBUSER,
     host: process.env.HOST,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: process.env.PORT,
-    ssl: true
 });
 
 module.exports = {
